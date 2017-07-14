eaa85466a1aabb87340a7dc2986cf9094b211ce6
yangjiang <jiangyang5157@gmail.com>
2017-07-14 21:32:57 +1200

    n

--- node-id.py
+++
@@ -1,7 +0,0 @@
-import tensorflow as tf
-
-a = tf.placeholder(tf.int32, shape=())
-b = tf.placeholder(tf.int32, shape=())
-add = tf.add(a, b)
-sess = tf.InteractiveSession()
-print(sess.run(add, feed_dict={a: 1, b: 2}))
